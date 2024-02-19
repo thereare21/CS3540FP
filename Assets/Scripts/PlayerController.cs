f44c480ace76e5e67266d82140fc61fb9e15f829
Raymond Tsai <arethere21@gmail.com>
2024-02-19 00:11:07 -0500

    Player Controller and Turret Aggro

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public float jumpHeight = 10;
+    public float gravity = 9.81f;
+
+    public float airControl = 10f;
+
+    CharacterController controller;
+    Vector3 input, moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+
+        input *= speed;
+
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else
+            {
+                moveDirection.y = 0.0f;
+            }
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TurretBehavior.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public float turnSpeed = 200f;
+
+
+    bool playerIsInRange = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsInRange)
+        {
+            //transform.LookAt(player.transform);
+            //transform.rotation = Vector3.Lerp(transform.rotation, transform.LookAt(player.transform); //can't be done :(
+
+            //Vector3 forward = transform.forward;
+            Vector3 relativePos = player.transform.position - transform.position;
+
+            //float angle = Vector3.Angle(relativePos, forward);
+
+            Quaternion lookRotation = Quaternion.LookRotation(relativePos);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player detected");
+        }
+
+        playerIsInRange = true;
+
+        //aggro towards player
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player has left range");
+        }
+
+        playerIsInRange = false;
+    }
+}
