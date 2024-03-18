76ad7d4339193e742b2b6719e7f4cd285cbfb14f
Jan Heinz <heinz.j@northeastern.edu>
2024-03-18 11:28:46 -0400

    added crouch mechanic and cleaned up player controller
    crouch is currently bound to left shift. left controller is another key for "Fire1" which we use so I had to avoid using it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,16 @@ public class PlayerController : MonoBehaviour
     public float airControl = 5f;
     public AudioClip[] footStepSFX; // array to allow variability in sfx
     public float footstepDelay = 0.5f; 
+    
+    // height variables for crouching and standing
+    public float standHeight = 2.0f; 
+    public float crouchHeight = 1.0f;
+    public Transform playerTransform;
+    public Transform cameraTransform;
+    public float standCameraHeight = 0.8f; 
+    public float crouchCameraHeight = 0.4f;
+
+    bool isCrouching = false;
 
     CharacterController controller;
     Vector3 input, moveDirection;
@@ -25,6 +35,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       HandleMovement();
+       HandleCrouch();
+    }
+
+    void HandleMovement() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
@@ -43,14 +58,7 @@ public class PlayerController : MonoBehaviour
                 moveDirection.y = 0.0f;
             }
 
-            // play footstep sfx when moving on the ground
-            if (input.magnitude > 0 && Time.time - lastFootstepTime > footstepDelay && footStepSFX.Length > 0)
-            {
-                lastFootstepTime = Time.time;
-                AudioSource.PlayClipAtPoint(footStepSFX[lastFootstepIndex], transform.position, 0.1f); 
-                // go to next sfx (or back to the beginning)
-                lastFootstepIndex = (lastFootstepIndex + 1) % footStepSFX.Length;
-            }
+            PlayFootstepSFX();
         }
         else
         {
@@ -60,5 +68,44 @@ public class PlayerController : MonoBehaviour
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void HandleCrouch() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            if (!isCrouching) {
+                Crouch();
+            }
+        }
+        else if (isCrouching) {
+            StandUp();
+        }
+    }
+
+    void Crouch() {
+        controller.height = crouchHeight;
+        cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, crouchCameraHeight,
+            cameraTransform.localPosition.z);
+        playerTransform.localScale = new Vector3(playerTransform.localScale.x, 0.5f, playerTransform.localScale.z);
+        isCrouching = true;
+    }
+
+    void StandUp() {
+        controller.height = standHeight;
+        cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, standCameraHeight, 
+            cameraTransform.localPosition.z);
+        playerTransform.localScale = new Vector3(playerTransform.localScale.x, 1f, playerTransform.localScale.z);
+
+        isCrouching = false;
+    }
+
+    void PlayFootstepSFX() {
+        // play footstep sfx when moving on the ground
+        if (input.magnitude > 0 && Time.time - lastFootstepTime > footstepDelay && footStepSFX.Length > 0)
+        {
+            lastFootstepTime = Time.time;
+            AudioSource.PlayClipAtPoint(footStepSFX[lastFootstepIndex], transform.position, 0.1f); 
+            // go to next sfx (or back to the beginning)
+            lastFootstepIndex = (lastFootstepIndex + 1) % footStepSFX.Length;
+        }
     }
 }
