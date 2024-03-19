89c5ae54003030979636bca4021a1238fdff253f
hakugakusha <87151999+hakugakusha@users.noreply.github.com>
2024-03-18 22:37:58 -0400

    added playerhealth & levelmanager
    added playerhealth and level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Text gameText;
+    public static bool isGameOver;
+    public string nextLevel;
+    public string currentLevel;
+    void Start()
+    {
+        isGameOver = false;
+        
+    }
+
+    public void LevelLost() {
+        isGameOver = true;
+        gameText.text = "GAME OVER!";
+        gameText.gameObject.SetActive(true);
+
+        if (!string.IsNullOrEmpty(nextLevel)) {
+            Invoke("LoadLevel", 2);
+        }
+    }
+
+    public void LevelBeat() {
+        isGameOver = true;
+        gameText.text = "YOU WIN!";
+        gameText.gameObject.SetActive(true);
+ 
+        if (!String.Equals(currentLevel, "LevelThree")) {
+            Invoke("LoadNextLevel", 2);
+        }
+
+    }
+
+    void LoadLevel() {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    void LoadNextLevel() {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    int currentHealth;
+    public Slider healthSlider;
+    void Start()
+    {
+        currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    public void TakeDamage(int damageAmount) {
+        if (currentHealth > 0) {
+            currentHealth -= damageAmount;
+            healthSlider.value = currentHealth;
+        }
+        if (currentHealth <= 0) {
+            PlayerDies();
+        }
+    }
+
+    void PlayerDies() {
+        transform.Rotate(-90, 0, 0, Space.Self);
+    }
+}
